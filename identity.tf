b25d4b37a582c5beb06b4ae49fe248340cc20bc0
ravcusat234 <112645977+ravcusat234@users.noreply.github.com>
2022-09-12 17:07:15 +0530

    Update identity.tf

--- identity.tf
+++ identity.tf
@@ -17,22 +17,10 @@ provider "okta" {
     base_url = var.base_url
     api_token = var.api_token
 }
-resource "okta_user_schema_property" "passport" {
-  index  = "passport"
-  title  = "Passport"
-  type   = "string"
-  master = "PROFILE_MASTER"
-}
 
 
-resource "okta_group" "example123" {
-  name        = "Example123"
-  description = "My Example Group123"
-}
-resource "okta_group" "testmyterraformgroup" {
-  name        = "testmyterraformgroup"
-  description = "Testing Terraform Group"
-}
+
+
 resource "okta_group" "testmyterraformgroup123" {
   name        = "testmyterraformgroup123"
   description = "Testing Terraform Group123"
