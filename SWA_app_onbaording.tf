d6828a402dafe0c7d0b7aead8b08bb69d6724d4a
ravcusat234 <112645977+ravcusat234@users.noreply.github.com>
2022-09-20 21:39:51 +0530

    Update SWA_app_onbaording.tf

--- SWA_app_onbaording.tf
+++ SWA_app_onbaording.tf
@@ -1,8 +1,25 @@
+variable "SWA_APP1_URL" {}
+
+#variable Declaration above
+
+
 resource "okta_app_swa" "SWAappUsingTF" {
   label          = "SWAappUsingTF"
   button_field   = "login"
   password_field = "password"
   username_field = "username"
   url            = "https://login.splunk.com"
+  accessibility_self_service= "true"
+}
+
+
+#code for App onbaording SWA app1
+
+resource "okta_app_swa" "SWAappUsingTFNew" {
+  label          = "SWAappUsingTFNew"
+  button_field   = "login"
+  password_field = "password"
+  username_field = "username"
+  url            = var.SWA_APP1_URL
   accessibility_self_service= "true"
 }
