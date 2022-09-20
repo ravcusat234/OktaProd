7cf807d08b96c262e973c5d0c3ddefc37febd1be
ravcusat234 <112645977+ravcusat234@users.noreply.github.com>
2022-09-20 17:55:58 +0530

    Add files via upload

---
+++ SWA_app_onbaording.tf
@@ -0,0 +1,8 @@
+resource "okta_app_swa" "SWAappUsingTF" {
+  label          = "SWAappUsingTF"
+  button_field   = "login"
+  password_field = "password"
+  username_field = "username"
+  url            = "https://login.splunk.com"
+  accessibility_self_service= "true"
+}
