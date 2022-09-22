4c78f74e41d7e5ce8d51d08614f36776f1114db0
ravcusat234 <112645977+ravcusat234@users.noreply.github.com>
2022-09-22 16:34:54 +0530

    Update CreateUser.tf

--- CreateUser.tf
+++ CreateUser.tf
@@ -1,5 +1,5 @@
 resource "okta_user" "UserCreateUsingTF" {
-  admin_roles = ["READ_ONLY_ADMIN"]
+  admin_roles = ["okta_user_admin_roles"]
   first_name  = "TF1"
   last_name   = "User"
   login       = "TF1@mailinatorcom"
